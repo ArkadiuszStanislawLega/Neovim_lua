eca30baf5d5c5dc245d68691ec1d10e39e1ecd9f
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2024-07-10 09:41:16 +0200

    Update - added formatting clang to cpp file on f12

--- lua/custom-keys.lua
+++
@@ -1,8 +0,0 @@
-local map = vim.api.nvim_set_keymap
-map('n', 'm', [[:NvimTreeToggle<CR>]], {})
-map('n', '<Space>', [[<C-w>w]], {})
-map('n', 'ss', [[:split<CR>]], {})
-map('n', 'sv', [[:vsplit<CR>]], {})
-
-map('n', '<F5>', [[:lua require'dap'.continue()<CR>]], {})
-map('n', '<C-b>', [[:lua require'dap'.toggle_breakpoint()<CR>]], {})
